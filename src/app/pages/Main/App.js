24c57d17faeebed83230656c3922a9d580479cf2
dlipartiti <dario.lipartiti@mercadolibre.com>
2021-07-15 16:04:43 -0300

    Remove some boilerplate unnecessary files and add node-sass to dev-dependencies

---
+++ src/app/pages/Main/App.js
@@ -0,0 +1,17 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+          CHALLENGE
+      </header>
+      <main className="app-main">ALL THE REST</main>
+      <footer className="app-footer">
+        Copyright 2021 - Dario Lipartiti
+      </footer>
+    </div>
+  );
+}
+
+export default App;
